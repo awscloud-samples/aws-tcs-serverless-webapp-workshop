dfda8a509ff56e0045f916ebbde9eb14c3a65dad
awscloud-samples <xiara.solutions@gmail.com>
2021-06-21 15:12:35 -0500

    text changed

--- resources/code/WildRydesVue/src/views/Unicorns.vue
+++ resources/code/WildRydesVue/src/views/Unicorns.vue
@@ -5,12 +5,12 @@
 
  <div class="row column medium-10 large-8 xxlarge-6">
     <p class="content">
-      Wild Rydes has a dedicated staff that recruits, trains, and tends to our herd of unicorns. We take great pride in the quality of unicorns and rydes that we provide to our customers, and our staff exercises the utmost care in vetting the unicorns that join our herd.
+      Wild Rydes has a dedicated staff that recruits, trains, and tends to our herd of cars. We take great pride in the quality of cars and rydes that we provide to our customers, and our staff exercises the utmost care in vetting the cars that join our herd.
     </p>
     <p class="content">
-      Every unicorn goes through a rigorous due diligence process where we perform background checks, flying exams, and several rounds of interviews. Unicorns accepted to Wild Rydes are then treated to the best care and maintenance possible. We provide them excellent benefits, health care, and employee perks. This is part of our company philosophy in which happy unicorns lead to happy customers.
+      Every car goes through a rigorous due diligence process where we perform background checks, flying exams, and several rounds of interviews. Cars accepted to Wild Rydes are then treated to the best care and maintenance possible. We provide them excellent benefits, health care, and employee perks. This is part of our company philosophy in which happy cars lead to happy customers.
     </p>
-    <p class="content">Meet a few of the unicorns that are part of our family.</p>
+    <p class="content">Meet a few of the cars that are part of our family.</p>
   </div>
 
   <section class="unicorns-list">
@@ -22,10 +22,10 @@
         </div>
 
         <div class="columns medium-7 large-6 xlarge-5 xxlarge-4">
-          <h2 class="title">Bucephalus</h2>
-          <div class="subtitle">Golden Swiss</div>
+          <h2 class="title">Tesla Model 3</h2>
+          <div class="subtitle">M3</div>
           <p class="content">
-            Bucephalus joined Wild Rydes in February 2016 and has been giving rydes almost daily. He says he most enjoys getting to know each of his ryders, which makes the job more interesting for him. In his spare time, Bucephalus enjoys watching sunsets and playing Pokemon Go.
+            Tesla Model 3 joined Wild Rydes in February 2016 and has been giving rydes almost daily. He says he most enjoys getting to know each of his ryders, which makes the job more interesting for him. In his spare time, Tesla model 3 enjoys watching sunsets and playing Pokemon Go.
           </p>
         </div>
       </div>
@@ -38,10 +38,10 @@
         </div>
 
         <div class="columns medium-7 medium-pull-5 large-6 large-pull-6 xlarge-5 xlarge-pull-5 xxlarge-4 xxlarge-offset-1">
-          <h2 class="title">Shadowfox</h2>
-          <div class="subtitle">Brown Jersey</div>
+          <h2 class="title">Hellcat</h2>
+          <div class="subtitle">Kitty</div>
           <p class="content">
-            Shadowfox joined Wild Rydes after completing a distinguished career in the military, where he toured the world in many critical missions. Shadowfox enjoys impressing his ryders with magic tricks that he learned from his previous owner.
+            Hellcat joined Wild Rydes after completing a distinguished career in the military, where he toured the world in many critical missions. Hellcat enjoys impressing his ryders with magic tricks that he learned from his previous owner.
           </p>
         </div>
       </div>
@@ -54,10 +54,10 @@
         </div>
 
         <div class="columns medium-7 large-6 xlarge-5 xxlarge-4">
-          <h2 class="title">Rocinante</h2>
-          <div class="subtitle">Baby Flying Yellowback</div>
+          <h2 class="title">Golf GTI</h2>
+          <div class="subtitle">The machine</div>
           <p class="content">
-            Rocinante recently joined the Wild Rydes team in Madrid, Spain. She was instrumental in forming Wild Rydes’ Spanish operations after a long, distinguished acting career in windmill shadow-jousting.
+            Golf GTI recently joined the Wild Rydes team in Madrid, Spain. He is instrumental in forming Wild Rydes’ Spanish operations after a long, distinguished acting career in windmill shadow-jousting.
           </p>
         </div>
       </div>
@@ -76,8 +76,8 @@ export default {
   name: 'unicorns',
   data(){
     return{
-      title:"Unicorns Are Our Friends",
-      content:"The app is what makes this service exist, but the unicorns make it move. Meet them and see who you are riding with!"
+      title:"Super Cars Are Our Friends",
+      content:"The app is what makes this service exist, but the cars make it move. Meet them and see who you are riding with!"
     }
   },
   components: {
